5c2c269429983ad59e859be0b64058387f216832
Rajiv Ramakrishnan <rajiv@buyproperly.ca>
2024-07-22 15:42:32 +0530

    feat: blocking public acces

--- buyproperly-admin-dashboard/production/pipeline_s3.tf
+++ buyproperly-admin-dashboard/production/pipeline_s3.tf
@@ -9,3 +9,13 @@ resource "aws_s3_bucket_versioning" "versioning_example" {
     status = "Enabled"
   }
 }
+
+resource "aws_s3_bucket_public_access_block" "example" {
+  bucket = aws_s3_bucket.app_bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
--- buyproperly-admin-dashboard/staging/app_s3.tf
+++ buyproperly-admin-dashboard/staging/app_s3.tf
@@ -64,3 +64,12 @@ resource "aws_s3_bucket_policy" "app_bucket_policy" {
 EOF
 }
 
+resource "aws_s3_bucket_public_access_block" "example" {
+  bucket = aws_s3_bucket.app_bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
