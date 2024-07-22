8a386acae2155252ac35f8b9100bdd38a69f656b
Rajiv Ramakrishnan <rajiv@buyproperly.ca>
2024-07-22 11:15:17 +0530

    feat: enabling versioning for pipeline buckets

--- buyproperly-admin-dashboard/production/pipeline_s3.tf
+++ buyproperly-admin-dashboard/production/pipeline_s3.tf
@@ -1,4 +1,11 @@
 resource "aws_s3_bucket" "pipeline_bucket" {
-  bucket = var.pipeline_bucket_name
+  bucket        = var.pipeline_bucket_name
   force_destroy = true
+}
+
+resource "aws_s3_bucket_versioning" "versioning_example" {
+  bucket = aws_s3_bucket.pipeline_bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
 }
